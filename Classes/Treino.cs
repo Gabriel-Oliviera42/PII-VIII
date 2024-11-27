a40b0a75e02bb3affd7a547c0f0f0e0ad2ce7003
LenilsonJunior <151935044+LenilsonJunior@users.noreply.github.com>
2024-11-27 07:42:14 -0300

    criação de classe para fazer a inserção de dados ou a remoção do treino no banco de dados

--- Classes/Treino.cs
+++ Classes/Treino.cs
@@ -28,6 +28,71 @@ namespace PII_VIII
             DataTable dt = conexao.RetornaTabela(query);
             return dt;
         }
+        
+public class TreinoRepository : IDisposable
+ {
+     private readonly SqlConnection _connection;
+     Conexao conexao = new Conexao();
 
+     public TreinoRepository()
+     {
+
+         try
+         {
+             // Inicialize sua conexão com uma string de conexão válida
+             conexao.Conectar();
+             _connection = conexao.Conn;
+         }
+         catch (Exception ex)
+         {
+             throw new InvalidOperationException("Erro ao inicializar a conexão com o banco de dados. Verifique a string de conexão.", ex);
+         }
+     }
+
+     public void AdicionarTreinoAoUsuario(int idUsuario, int idTreino)
+     {
+         try
+         {
+             var query = "INSERT INTO treino_usuario (id_usuario, id_treino) VALUES (@IdUsuario, @IdTreino)";
+             using (var command = new SqlCommand(query, _connection))
+             {
+                 command.Parameters.AddWithValue("@IdUsuario", idUsuario);
+                 command.Parameters.AddWithValue("@IdTreino", idTreino);
+                 command.ExecuteNonQuery();
+             }
+         }
+         catch (Exception ex)
+         {
+             throw new InvalidOperationException("Erro ao adicionar treino ao usuário no banco de dados.", ex);
+         }
+     }
+
+     public void RemoverTreinoDoUsuario(int idUsuario, int idTreino)
+     {
+         try
+         {
+             var query = "DELETE FROM treino_usuario WHERE id_usuario = @idUsuario AND id_treino = @IdTreino";
+             using (var command = new SqlCommand(query, _connection))
+             {
+                 command.Parameters.AddWithValue("@IdUsuario", idUsuario);
+                 command.Parameters.AddWithValue("@IdTreino", idTreino);
+                 command.ExecuteNonQuery();
+             }
+         }
+         catch (Exception ex)
+         {
+             throw new InvalidOperationException("Erro ao remover treino do usuário no banco de dados.", ex);
+         }
+     }
+
+     public void Dispose()
+     {
+         if (_connection != null && _connection.State != System.Data.ConnectionState.Closed)
+         {
+             _connection.Close();
+         }
+         _connection?.Dispose();
+     }
+ }
     }
 }
